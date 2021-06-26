ff72d68583dacb37d7be2ad0437f08dad8f1eb80
GongT <admin@gongt.me>
2021-06-26 17:47:17 +0800

    update shell

--- applications/shell/relay_shell.c
+++ applications/shell/relay_shell.c
@@ -3,9 +3,9 @@
 static long set_relay_msh(int argc, char **argv)
 {
 	if (str_eq(argv[1], "on"))
-		relay_set(1);
+		relay_set(RELAY_ON);
 	else if (str_eq(argv[1], "off"))
-		relay_set(0);
+		relay_set(RELAY_OFF);
 	else
 		RETURN_WITH_ERR("relay on/off");
 	return 0;
